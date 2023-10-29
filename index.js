e4a227b6a0011fa39b04256d2dd4cc8e0600d277
Huw Prosser <huw@carterapi.com>
2023-10-29 19:24:54 +0000

    Adds auto-nav

--- index.js
+++ index.js
@@ -10,25 +10,62 @@ document.addEventListener("DOMContentLoaded", function () {
             const postsList = document.querySelector("ul");
             postsList.innerHTML = "";
 
-            const posts = directories.map((directory) => {
-                let title = directory.name.split("-").slice(3).join("-");
-                let date = directory.name.split("-").slice(0, 3).join("-");
+            let directoryName = directories[0].name;
+            let file = `/posts/${directoryName}/index.html`;
 
-                const listItem = document.createElement("li");
-                const postLink = document.createElement("a");
+            // read the file's title tag
+            fetch(file)
+                .then((response) => {
+                    return response.text();
+                })
+                .then((html) => {
+                    let parser = new DOMParser();
+                    let doc = parser.parseFromString(html, "text/html");
+                    let title = doc.querySelector("title").innerText;
+                    // get date using the post's meta tag
+                    let date = doc.querySelector("meta[name='date']").content;
 
-                postLink.href = `/${DIR}/${directory.name}/`;
-                postLink.innerText = `${title.replace(/-/g, " ")}`;
+                    const listItem = document.createElement("li");
+                    const postLink = document.createElement("a");
+                    const span = document.createElement("span");
 
-                listItem.appendChild(postLink);
+                    postLink.href = `/${DIR}/${directoryName}/`;
+                    postLink.innerText = `${title}`;
 
-                let span = document.createElement("span");
-                span.classList.add("date");
-                span.innerText = ` (${date.replace(/-/g, "/")})`;
+                    span.classList.add("date");
+                    span.innerText = ` ${new Date(date).toLocaleDateString(
+                        "en-GB",
+                        {
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                        }
+                    )}`;
 
-                listItem.appendChild(span);
-                postsList.appendChild(listItem);
-            });
+                    listItem.appendChild(postLink);
+                    listItem.appendChild(span);
+                    postsList.appendChild(listItem);
+                });
+
+            // const posts = directories.map((directory) => {
+            //     let title = directory.name.split("-").slice(3).join("-");
+            //     let date = directory.name.split("-").slice(0, 3).join("-");
+
+            //     const listItem = document.createElement("li");
+            //     const postLink = document.createElement("a");
+
+            //     postLink.href = `/${DIR}/${directory.name}/`;
+            //     postLink.innerText = `${title.replace(/-/g, " ")}`;
+
+            //     listItem.appendChild(postLink);
+
+            //     let span = document.createElement("span");
+            //     span.classList.add("date");
+            //     span.innerText = ` (${date.replace(/-/g, "/")})`;
+
+            //     listItem.appendChild(span);
+            //     postsList.appendChild(listItem);
+            // });
 
             return Promise.all(posts);
         })
---
+++ posts/post.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Extract the title from the <title> tag
+    let title = document.querySelector("title").innerText;
+
+    // Extract the post date from the <meta name="date"> tag
+    let dateContent = document
+        .querySelector("meta[name='date']")
+        .getAttribute("content");
+    let dateParts = dateContent.split("-");
+    let formattedDate = `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
+
+    // Construct the head div
+    let headDiv = document.createElement("div");
+    headDiv.className = "head";
+
+    let backButton = document.createElement("a");
+    backButton.href = "/";
+    backButton.innerHTML = "← posts";
+    headDiv.appendChild(backButton);
+
+    let postTitle = document.createElement("h1");
+    postTitle.innerText = title;
+    headDiv.appendChild(postTitle);
+
+    let postDate = document.createElement("p");
+    postDate.className = "date";
+    postDate.innerText = formattedDate;
+    headDiv.appendChild(postDate);
+
+    // Insert the head div at the top of the .post-body div
+    let postBody = document.querySelector(".post-body");
+    postBody.insertBefore(headDiv, postBody.firstChild);
+});
