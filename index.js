0da0bf6698f4fc92814973d37985cf82eec3254f
Huw Prosser <huw@carterapi.com>
2023-10-29 18:58:06 +0000

    Adds auto list gen

--- index.js
+++ index.js
@@ -2,38 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const USER = "huwprosser";
     const REPO = "huwprosser.github.io";
     const DIR = "posts";
-    const API_ENDPOINT = `https://api.github.com/repos/${USER}/${REPO}/${DIR}`;
+    const API_ENDPOINT = `https://api.github.com/repos/${USER}/${REPO}/contents/${DIR}`;
 
     fetch(API_ENDPOINT)
         .then((response) => response.json())
         .then((directories) => {
             const postsList = document.querySelector("ul");
-            postsList.innerHTML = ""; // Clear the list first
+            postsList.innerHTML = "";
 
-            const postPromises = directories.map((directory) => {
-                return fetch(directory.git_url)
-                    .then((response) => response.json())
-                    .then((data) => {
-                        const content = atob(data.content); // Decode base64 content
-                        const titleMatch = content.match(
-                            /<title>([^<]+)<\/title>/
-                        ); // Extract title from content
+            const posts = directories.map((directory) => {
+                let title = directory.name.split("-").slice(3).join("-");
+                let date = directory.name.split("-").slice(0, 3).join("-");
 
-                        if (titleMatch && titleMatch[1]) {
-                            const listItem = document.createElement("li");
-                            const postLink = document.createElement("a");
+                const listItem = document.createElement("li");
+                const postLink = document.createElement("a");
 
-                            postLink.href = `/${DIR}/${directory.name}/`;
-                            postLink.innerText = titleMatch[1];
+                postLink.href = `/${DIR}/${directory.name}/`;
+                postLink.innerText = `${title.replace("-", " ")} (${date})`;
 
-                            listItem.appendChild(postLink);
-                            postsList.appendChild(listItem);
-                            console.log(`Added post: ${titleMatch[1]}`);
-                        }
-                    });
+                listItem.appendChild(postLink);
+                postsList.appendChild(listItem);
             });
 
-            return Promise.all(postPromises);
+            return Promise.all(posts);
         })
         .catch((error) => console.error("Error fetching posts:", error));
 });
